8e3cd6be221afbfa72fd055523cd77d564a6de0a
Felipe Madureira de Souza <Felippefms3@gmail.com>
2024-03-05 18:07:17 -0300

    Update requests.js

--- app/api/requests.js
+++ app/api/requests.js
@@ -1,15 +1,12 @@
 import { instance } from "./api";
 
 export function GetAllUsers() {
-
-    if (StorageService.isAdminLoggedIn() === true) {
-        instance.get('/heroes')
-            .then((response) => {
-                const usersList = response.data
-                console.log(usersList);
-            })
-            .catch(() => {
-                console.log();
-            });
-    }
+    instance.get('/heroes')
+        .then((response) => {
+            const usersList = response.data
+            console.log(usersList);
+        })
+        .catch(() => {
+            console.log();
+        });
 };
